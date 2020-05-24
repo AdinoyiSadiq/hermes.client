2b91ab8e43b9d458c27e3be2bcf615b0f692d496
Adinoyi Sadiq <sadiqadinoyi@gmail.com>
2020-05-24 11:34:44 +0100

    refactor signup form to use hooks

--- src/components/auth/SignupForm.js
+++ src/components/auth/SignupForm.js
@@ -1,190 +1,168 @@
-import React, { Component } from 'react';
-import { Mutation } from '@apollo/react-components';
+import React, { useState }  from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import FormInput from '../forms/FormInput';
 import SIGNUP_MUTATION from '../../mutations/signup';
 import Button from '../buttons/Button';
 import validateAuth from '../../lib/validation';
 
 const fieldNames = ['firstname', 'lastname', 'username', 'location', 'email', 'password', 'confirmPassword'];
-// refacrot to use hooks
-class SignupForm extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      firstname: '',
-      lastname: '',
-      username: '',
-      location: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      touched: {
-        firstname: false,
-        lastname: false,
-        username: false,
-        location: false,
-        email: false,
-        password: false,
-        confirmPassword: false,
-      },
-    }
+const SignupForm = (props) => {
+  const [touchedInput, setTouchedInput] = useState({});
+  const [inputValue, setInputValue] = useState({
+    firstname: '',
+    lastname: '',
+    username: '',
+    location: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const [signup, { loading, error, data, client }] = useMutation(SIGNUP_MUTATION);
+
+  const handleChange = (event) => {
+    setInputValue({ ...inputValue, [event.target.id]: event.target.value });
   }
 
-  handleChange = (event) => {
-    this.setState({ [event.target.id]: event.target.value });
+  const handleBlur = (field) => () => {
+    setTouchedInput({ ...touchedInput, [field]: true })
   }
 
-  handleBlur = (field) => () => {
-    const { touched } = this.state;
-    this.setState({
-      touched: { ...touched, [field]: true },
-    });
-  }
-
-  changeTouchState = (fields, state) => {
-    const touchState = {};
-    fields.forEach((field) => {
-      touchState[field] = state;
-    });
-    return touchState;
-  }
-
-  async handleSubmit(event, signup) {
+  const handleSubmit = async (event) => {
     event.preventDefault(); 
     const {
       firstname, lastname, username, location, email, password, confirmPassword
-    } = this.state;
+    } = inputValue;
 
     const validationError = validateAuth({
       firstname, lastname, username, location, email, password, confirmPassword
     }, fieldNames);
-    this.changeTouchState(fieldNames, true);
+
     if (!validationError.status) {
-      const res = await signup();
-      if (res && res.data && res.data.signup && res.data.signup.token) {
-        localStorage.setItem('authToken', res.data.signup.token);
-        this.props.history.push('/home');
-      }
-    }
-  }
-
-  render() {
-    const {
-      firstname, lastname, username, location, email, password, confirmPassword, touched
-    } = this.state;
-
-    const validationError = validateAuth({
-      firstname, lastname, username, location, email, password, confirmPassword
-    }, fieldNames);
-    return (
-      <Mutation 
-        mutation={SIGNUP_MUTATION}
-        variables={{
+      await signup({
+        variables: {
           firstname: firstname.toLowerCase().trim(), 
           lastname: lastname.toLowerCase().trim(), 
           username: username.toLowerCase().trim(), 
           location: location.toLowerCase().trim(), 
           email: email.toLowerCase().trim(), 
-          password }}
-      >
-        {(signup, { loading, error }) => (
-          <form className='form u-margin-bottom-4'
-            onSubmit={event => {
-            this.handleSubmit(event, signup)
-          }}>
-            <div className='form__row'>
-              <FormInput 
-                error={validationError}
-                touched={touched}
-                value={firstname}
-                handleChange={this.handleChange}
-                handleBlur={this.handleBlur('firstname')}
-                name='firstname'
-                placeholder='First Name'
-              />
-              <FormInput 
-                error={validationError}
-                touched={touched}
-                value={lastname}
-                handleChange={this.handleChange}
-                handleBlur={this.handleBlur('lastname')}
-                name='lastname'
-                placeholder='Last Name'
-              />
-            </div>
-            <div className='form__row'>
-              <FormInput
-                error={validationError} 
-                touched={touched}
-                value={username}
-                handleChange={this.handleChange}
-                handleBlur={this.handleBlur('username')}
-                name='username'
-                placeholder='User Name'
-              />
-              <FormInput 
-                error={validationError}
-                touched={touched}
-                value={location}
-                handleChange={this.handleChange}
-                handleBlur={this.handleBlur('location')}
-                name='location'
-                placeholder='Location'
-              />
-            </div>
-            <FormInput 
-              error={validationError}
-              touched={touched}
-              value={email}
-              handleChange={this.handleChange}
-              handleBlur={this.handleBlur('email')}
-              name='email'
-              placeholder='Email'
-            />
-            <div className='form__row'>
-              <FormInput 
-                error={validationError}
-                touched={touched}
-                value={password}
-                handleChange={this.handleChange}
-                handleBlur={this.handleBlur('password')}
-                type="password"
-                name='password'
-                placeholder='Password'
-              />
-              <FormInput 
-                error={validationError}
-                touched={touched}
-                value={confirmPassword}
-                handleChange={this.handleChange}
-                handleBlur={this.handleBlur('confirmPassword')}
-                type="password"
-                name='confirmPassword'
-                placeholder='Confirm Password'
-              />
-            </div>
-            <Button
-              type='submit'
-              disabled={
-                loading || 
-                !firstname.trim() || 
-                !lastname.trim() || 
-                !username.trim() || 
-                !location.trim() || 
-                !email.trim() || 
-                !password.trim() || 
-                !confirmPassword.trim()
-              }
-            >
-              Sign Up
-            </Button>
-            {error && <div>{error.message}</div>}
-          </form>
-        )}
-      </Mutation>
-    );
+          password
+        }
+      });
+    }
   }
+
+  if (data && data.signup && data.signup.token) {
+    client.writeData({ data: { isAuth: true } });
+    localStorage.setItem('authToken', data.signup.token);
+    props.history.push('/home');
+  }
+
+    const validationError = validateAuth({
+      firstname: inputValue.firstname, 
+      lastname: inputValue.lastname, 
+      username: inputValue.username, 
+      location: inputValue.location,
+      email: inputValue.email, 
+      password: inputValue.password, 
+      confirmPassword: inputValue.confirmPassword
+    }, fieldNames);
+
+    return (
+      <form className='form u-margin-bottom-4'
+        onSubmit={event => {
+        handleSubmit(event)
+      }}>
+        <div className='form__row'>
+          <FormInput 
+            error={validationError}
+            touched={touchedInput.firstname}
+            value={inputValue.firstname}
+            handleChange={handleChange}
+            handleBlur={handleBlur('firstname')}
+            name='firstname'
+            placeholder='First Name'
+          />
+          <FormInput 
+            error={validationError}
+            touched={touchedInput.lastname}
+            value={inputValue.lastname}
+            handleChange={handleChange}
+            handleBlur={handleBlur('lastname')}
+            name='lastname'
+            placeholder='Last Name'
+          />
+        </div>
+        <div className='form__row'>
+          <FormInput
+            error={validationError} 
+            touched={touchedInput.username}
+            value={inputValue.username}
+            handleChange={handleChange}
+            handleBlur={handleBlur('username')}
+            name='username'
+            placeholder='User Name'
+          />
+          <FormInput 
+            error={validationError}
+            touched={touchedInput.location}
+            value={inputValue.location}
+            handleChange={handleChange}
+            handleBlur={handleBlur('location')}
+            name='location'
+            placeholder='Location'
+          />
+        </div>
+        <FormInput 
+          error={validationError}
+          touched={touchedInput.email}
+          value={inputValue.email}
+          handleChange={handleChange}
+          handleBlur={handleBlur('email')}
+          name='email'
+          placeholder='Email'
+        />
+        <div className='form__row'>
+          <FormInput 
+            error={validationError}
+            touched={touchedInput.password}
+            value={inputValue.password}
+            handleChange={handleChange}
+            handleBlur={handleBlur('password')}
+            type="password"
+            name='password'
+            placeholder='Password'
+          />
+          <FormInput 
+            error={validationError}
+            touched={touchedInput.confirmPassword}
+            value={inputValue.confirmPassword}
+            handleChange={handleChange}
+            handleBlur={handleBlur('confirmPassword')}
+            type="password"
+            name='confirmPassword'
+            placeholder='Confirm Password'
+          />
+        </div>
+        <Button
+          type='submit'
+          disabled={
+            loading || 
+            !inputValue.firstname.trim() || 
+            !inputValue.lastname.trim() || 
+            !inputValue.username.trim() || 
+            !inputValue.location.trim() || 
+            !inputValue.email.trim() || 
+            !inputValue.password.trim() || 
+            !inputValue.confirmPassword.trim()
+          }
+        >
+          Sign Up
+        </Button>
+        {error && <div>{error.message}</div>}
+      </form>
+    );
 }
 
 export default SignupForm;
